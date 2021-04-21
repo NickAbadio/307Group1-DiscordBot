2ed33be93fa1465d6a27d86e907ecb546c4a3235
NickAbadio <na354@njit.com>
2021-04-21 15:22:22 -0400

    Git hub testing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 from dotenv import load_dotenv, find_dotenv
 
+#Git hub Testing
 load_dotenv(find_dotenv())
 
 client = discord.Client()
